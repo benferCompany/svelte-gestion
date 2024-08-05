e9eca7326cad8b425638a9876ae901ed21dcf3d4
Lautaro Flores <lautyflowers07@gmail.com>
2024-08-05 00:33:50 -0300

    Primer avance de la tienda

--- src/shop/contenidoTienda/Index.svelte
+++ src/shop/contenidoTienda/Index.svelte
@@ -1,1 +1,17 @@
-<h1>hello</h1>
+<script>
+    import Nav from "./nav/Nav.svelte";
+    let variable  = 51;
+
+</script>
+
+<!--Nav-->
+<Nav/>
+<h1>
+    Esto es el numero de la variable {variable}
+</h1>
+<!--Contenido-->
+
+<!--Footer-->
+<style>
+
+</style>
---
+++ src/shop/contenidoTienda/nav/Nav.svelte
@@ -0,0 +1,7 @@
+<h1>Esto es el nav</h1>
+
+<style>
+    h1{
+        color:red;
+    }
+</style>
