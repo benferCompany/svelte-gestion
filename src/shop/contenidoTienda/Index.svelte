d17b79b39a3fb870a48b878a98b3e553885cf430
Benjamín <benjidfer@gmail.com>
2024-08-12 09:04:41 -0300

    shop

--- src/shop/contenidoTienda/Index.svelte
+++ src/shop/contenidoTienda/Index.svelte
@@ -1,1 +1,24 @@
-<h1>hello</h1>
+<script>
+    import Nav from "./nav/Nav.svelte";
+    import Search from "./search/Search.svelte";
+    import Destacados from "./destacados/Destacados.svelte";
+    import Slider from "./slider/Slider.svelte";
+</script>
+
+<div class="index">
+    <Nav />
+    <Search />
+    <Slider />
+    <Destacados/>
+</div>
+
+<style>
+    .index {
+        background: rgb(241, 239, 239);
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
