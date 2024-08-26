d59da0b678508607423c5b246d9ee04f92273b51
Lautaro Flores <lautyflowers07@gmail.com>
2024-08-26 09:49:24 -0300

    push de lauty

--- src/shop/contenidoTienda/form/Form.svelte
+++ src/shop/contenidoTienda/form/Form.svelte
@@ -1,37 +1,4 @@
 <script>
-<<<<<<< HEAD
-    import { Link } from "svelte-routing";
-    import { accessGoogle } from "./form";
-    let styleVolver = `padding-top: 25px;
-    margin-left: 10px;
-    font-size: 20px;
-    color: white;
-    font-family: 'Arial Narrow', Arial, sans-serif;`;
-    import { booleanPathName } from "../../../components/tools/pathName/pathName";
-    import Footer from "../footer/Footer.svelte";
-    $booleanPathName = false;
-</script>
-
-<link
-    rel="stylesheet"
-    href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-/>
-<div class="body">
-    <div class="nav">
-        <Link to="/shop" style={styleVolver} class="volver">Volver</Link>
-        <img
-            src="https://firebasestorage.googleapis.com/v0/b/cloud-image-361ff.appspot.com/o/images%2FbenfershopPng.png?alt=media&token=e58609b9-6974-49f1-bb0d-d872f93f7eaf"
-            alt=""
-            class="logo"
-        />
-    </div>
-    <div class="contenedor">
-        <h1>¡Bienvenido a BenFer Shop!</h1>
-        <p>Estamos emocionados de tenerte aquí. Prepárate para descubrir nuestras ofertas exclusivas y productos únicos.</p>
-        <button>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyAzgLs4C8wzj7OXS5I4e2r_ZGarlAlq5_xA&s" 
-            alt="" class="img-btn">iniciar con google</button>
-=======
     import { Link, navigate } from "svelte-routing";
     import { IniciaSesion, accesGoogle } from "./form";
     import { onMount } from "svelte";
@@ -42,10 +9,8 @@
         margin-left: 10px;
         font-size: 20px;
         color: white;
-        font-family: 'Arial Narrow', Arial, sans-serif;
-    `;
-
-    // Manejador para el inicio de sesión
+        font-family: 'Arial Narrow', Arial`
+        
     async function handleSubmit(event) {
         try {
             await IniciaSesion(event);
@@ -68,7 +33,7 @@
     }
 </script>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
 <div class="body">
     <div class="nav">
@@ -106,7 +71,6 @@
                 </form>
             </fieldset>
         </div>
->>>>>>> 81aabd8bc13cc946bbb75bcf5de6ebe737ed2f75
     </div>
 </div>
 
@@ -116,10 +80,6 @@
         padding: 0;
         margin: 0;
         width: 100%;
-<<<<<<< HEAD
-        height: 100%;
-=======
->>>>>>> 81aabd8bc13cc946bbb75bcf5de6ebe737ed2f75
         position: absolute;
         top: 0;
         left: 0;
@@ -139,47 +99,6 @@
     }
 
     .contenedor {
-<<<<<<< HEAD
-        height: 100%;
-        text-align: center;
-        padding: 40px;
-        border-radius: 5px;
-        background: linear-gradient(135deg, #298dff, #011e29);
-        color: white;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-    }
-
-    h1 {
-            margin-bottom: 10px;
-        }
-
-    p {
-            margin-bottom: 20px;
-        }
-
-    button {
-        justify-content: space-between;
-        padding-right: 0px;    
-        margin-top: 30px;
-        background-color: #ffffff;
-        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.579);
-        color: rgb(2, 2, 2);
-        border: none;
-        padding: 15px 30px;
-        border-radius: 10px;
-        font-size: 16px;
-        cursor: pointer;
-        transition: background-color 0.3s;
-    }
-
-    .img-btn {
-        height: 20px;
-        width: auto;
-        margin-left: 0px;
-        margin-right: 8px;
-        
-    }
-=======
         position: relative;
         height: 100vh;
     }
@@ -266,5 +185,4 @@
     }
 
    
->>>>>>> 81aabd8bc13cc946bbb75bcf5de6ebe737ed2f75
 </style>
