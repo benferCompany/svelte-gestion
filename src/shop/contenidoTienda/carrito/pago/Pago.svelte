7782f8c4f58dbe0fd5de9b4776eb6bb3ae9b81e7
=Benjamin <bejidfer@gmail.com>
2024-10-08 19:22:56 -0300

    arreglo de mercadopago

--- src/shop/contenidoTienda/carrito/pago/Pago.svelte
+++ src/shop/contenidoTienda/carrito/pago/Pago.svelte
@@ -27,7 +27,7 @@
             await getCompra(detalle);
             // Inicializa MercadoPago una vez que el script esté cargado
             const mp = new window.MercadoPago(
-                "APP_USR-47614bc3-a0d2-4220-998c-7e261aa1d7d8",
+                "APP_USR-9b776e89-9e92-46f3-b0a2-d24b3cc83f73",
                 {
                     locale: "es-AR", // Cambia el locale según sea necesario
                 },
