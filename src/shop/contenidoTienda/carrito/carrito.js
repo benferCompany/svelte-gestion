d33d346ea32b467858f19d7a7485f6a5f4e3466e
=Benjamin <bejidfer@gmail.com>
2024-10-10 20:12:59 -0300

    arreglos

--- src/shop/contenidoTienda/carrito/carrito.js
+++ src/shop/contenidoTienda/carrito/carrito.js
@@ -4,7 +4,6 @@ let datosCarrito = localStorage.getItem("carrito") ? JSON.parse(localStorage.get
 export const carrito = writable(datosCarrito);
 
 export const datos = {
-    "id": 1,
     "cae": "",
     "caeFchVto": null,
     "salesPerson": {
