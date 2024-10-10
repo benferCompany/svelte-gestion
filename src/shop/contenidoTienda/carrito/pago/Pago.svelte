6fa227df52a1da0a9d6612c2ac84b58ebb6e2d88
=Benjamin <bejidfer@gmail.com>
2024-10-10 19:47:11 -0300

    arreglos

--- src/shop/contenidoTienda/carrito/pago/Pago.svelte
+++ src/shop/contenidoTienda/carrito/pago/Pago.svelte
@@ -49,7 +49,7 @@
 <div
     on:click={async () => {
         booleanMercadoPago = false;
-        detalle.idStatePayment = id.id;
+        detalle.idStatePayment = id;
         const response = await createDetails(detalle);
         if (Android && response) {
             Android.getToastMessage("Se guardo la compra.");
